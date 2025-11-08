14a5c4be5b208a212819ec1bb108c624f7c999f4
Amir <ezio.rzn80@gmail.com>
2025-11-08 19:33:51 +0330

    update backend

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import MarketDataView , LatestPriceView
 
 urlpatterns = [
-    # مسیر دریافت کندل‌ها (crypto یا forex)
     path('<str:market_type>/', MarketDataView.as_view(), name='market-data'),
     path('<str:market_type>/latest_price/', LatestPriceView.as_view(), name='latest-price'),
 ]
--- backend/market/views.py
+++ backend/market/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 import requests
 from django.conf import settings
 from .services.binance_service import get_crypto_candles
-from .services.forex_service import get_forex_candles  # ← توجه کن فایل forex_service.py باشه
+from .services.forex_service import get_forex_candles  
 from .services.http import get_json
 from datetime import datetime, timedelta
 import time
@@ -19,15 +19,12 @@ class MarketDataView(APIView):
             return Response({"error": "Symbol parameter is required"}, status=400)
 
         if market_type == "crypto":
-            # فراخوانی سرویس Mock کریپتو
             data = get_crypto_candles(symbol, interval)
         elif market_type == "forex":
-            # فراخوانی سرویس Mock فارکس
             data = get_forex_candles(symbol, interval)
         else:
             return Response({"error": "Invalid market type"}, status=400)
 
-        # داده‌های Mock شده به صورت { 'candles': [...] } برگردانده می‌شوند
         return Response(data, status=status.HTTP_200_OK)
 
 
@@ -37,17 +34,11 @@ class LatestPriceView(APIView):
         if not symbol:
             return Response({"error": "Symbol parameter is required"}, status=400)
 
-        # ----------------------------
-        # کریپتو: از Binance
-        # ----------------------------
         if market_type == "crypto":
             from .services.binance_service import get_latest_price
             price = get_latest_price(symbol)
             return Response(price, status=200)
 
-        # ----------------------------
-        # فارکس: از TwelveData
-        # ----------------------------
         elif market_type == "forex":
             key = settings.TWELVEDATA_KEY
             url = f"https://api.twelvedata.com/price?symbol={symbol}&apikey={key}"
